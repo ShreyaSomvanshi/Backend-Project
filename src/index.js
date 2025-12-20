7da641d37aafadeb1b7a3e9011da074da664cb41
ShreyaSomvanshi <shreyamisthi@gmail.com>
2025-12-20 17:50:48 +0530

    commit project setup

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed!!",err);
+})
