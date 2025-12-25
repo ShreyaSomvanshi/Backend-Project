d97a3e6d852ec3d7627d2c8e5b6f80f84c696c71
ShreyaSomvanshi <shreyamisthi@gmail.com>
2025-12-25 21:34:01 +0530

    commit routes

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB= async ()=>{
         console.log(`\n MongoDB connected !! DB HOST:${connectionInstance.connection.host}`)
     }
     catch(error){
-        console.log("MongoDB connection error", errror)
+        console.log("MongoDB connection error", error)
         process.exit(1)
     }
 }
