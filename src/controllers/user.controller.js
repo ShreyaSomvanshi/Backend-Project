741cb157b8e7e81e0cedf96e8ab620bd6b492fc7
ShreyaSomvanshi <shreyamisthi@gmail.com>
2026-01-08 17:59:59 +0530

    commit auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,7 @@ const generateAccessandRefreshToken = async (userId) => {
     try {
         const user = await User.findById(userId)
         const accessToken = user.generateAccessToken()
-        const aefreshToken = user.generateRefreshToken()
+        const refreshToken = user.generateRefreshToken()
 
         user.refreshToken = refreshToken
         await user.save({validateBeforeSave: false})
@@ -105,4 +105,30 @@ const loginUser = asyncHandler(async (req,res) =>{
     )
 }) 
 
-export {registerUser};
+const logoutUser = asyncHandler( async (req,res)=>{
+    await User.findByIdAndUpdate(
+        req.userId._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User Logged Out"))
+
+})
+
+export {registerUser,loginUser,logoutUser};
