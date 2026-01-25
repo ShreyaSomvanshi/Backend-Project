7d633309268512a9ec7d50c7d27406cb06636c63
ShreyaSomvanshi <shreyamisthi@gmail.com>
2026-01-25 15:08:57 +0530

    commit folder typo

--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -1,6 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../model/tweet.model.js"
-import {User} from "../model/user.model.js"
+import {Tweet} from "../models/tweet.model.js"
+import {User} from "../models/user.model.js"
 import {ApiError} from "../utilities/ApiError.js"
 import {ApiResponse} from "../utilities/ApiResponse.js"
 import {asyncHandler} from "../utilities/asyncHandler.js"
